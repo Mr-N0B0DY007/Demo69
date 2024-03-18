32843e580cd47aca5aa764800943fdaa5a3c7ac4
Bobby Gupta <140632308+Mr-N0B0DY007@users.noreply.github.com>
2024-03-18 21:21:03 +0530

    Add files via upload

---
+++ src/components/Netflixfile.jsx
@@ -0,0 +1,301 @@
+import React from "react";
+
+const Netflixfile = () => {
+  return (
+    <>
+      <div class="topnav">
+        <h1>All Netflix Series Download From Here</h1>
+      </div>
+
+      <div class="cards">
+        <div class="image">
+          <img
+            src="https://occ-0-4995-2164.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABRh8uaXgE7ndf2NUETlakDrmVSVswntKv6iCL2c4U3_3Ndv8eNbiHyIHJxE_8oHE8Ork4NP2FTTPGBJCwwaTcKX2EKcGqE5R9znYF699Jk3nsjHih5N5sW_3L1ohHJ7tyxrXmZwmPZA8l01KlYhVjEys0NfRfKyCIO_oOzhWwxPNAWXG9s6dF2zTZhPcLSafQ2G84WvOAhlNvB0kNeUx4-h852wGU0HTo4tuNkNJ8IaKqlp69D-G8n16xDdpvnkECIIRhGdY4ggE4VbglKft56m2BvCrC9pI96HX1EpVSOPJfMvWw0_EcbmcHU1vP3e6Fa0I7Y5pSIvE-4EweJBkzPw-ybjmME3XNZw_-woQ4NU3kEvnCOM.jpg?r=09b"
+            alt="placeholder"
+            height="200px"
+            width="100%"
+          />
+        </div>
+        <div class="title">
+          <p>Doli Saja Ke Rakhna</p>
+        </div>
+        <div class="des">
+          <p>This is Netflix Series</p>
+          <button>Watch now</button>
+        </div>
+      </div>
+
+      <div class="cards">
+        <div class="image">
+          <img
+            src="https://occ-0-4995-2164.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABUmLgKmFrl2Tq5jDmIBIdp-L6_fRFsTE7KslWARHa4Yb8f8q32MOBiACOoYAGFn4k8Pc_1P7QlN-F41Btkx4q8bp2gg0yA4kCWxamkIcyRVtyIoln6PDWQVi1_IAZHe4PALj.jpg?r=326"
+            alt="placeholder"
+            height="200px"
+            width="100%"
+          />
+        </div>
+        <div class="title">
+          <p>Doli Saja Ke Rakhna</p>
+        </div>
+        <div class="des">
+          <p>This is Netflix Series</p>
+          <button>Watch now</button>
+        </div>
+      </div>
+
+      <div class="cards">
+        <div class="image">
+          <img
+            src="https://occ-0-4995-2164.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABURO1L3trQRai2nuUMVdTmqfyeWWrjnwqWrFF-G8f461NYv45nuQlM1GYkBSZ8_ThsKt-0EMJpF1bl1Sm9y_oPG3HMTHKLSySJpHZ0YOZ374RoT9qE4JWWeMkbFMQ19FP10S.jpg?r=4f5"
+            alt="placeholder"
+            height="200px"
+            width="100%"
+          />
+        </div>
+        <div class="title">
+          <p>Doli Saja Ke Rakhna</p>
+        </div>
+        <div class="des">
+          <p>This is Netflix Series</p>
+          <button>Watch now</button>
+        </div>
+      </div>
+
+      <div class="cards">
+        <div class="image">
+          <img
+            src="https://occ-0-4995-2164.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABdzLf-Q3OK2H03_O-TYenAtdtgEP9kHEK4Wov-JlvcJNPMiTwFtyZQ_eJ8yMtVrYzoc6mSbEyFAgqu9z-5P0-htUTbfc1AMpCtffuTkpgSJ_c8nM2GKlmkpcEHqoMsmfAmDY.jpg?r=501"
+            alt="placeholder"
+            height="200px"
+            width="100%"
+          />
+        </div>
+        <div class="title">
+          <p>Doli Saja Ke Rakhna</p>
+        </div>
+        <div class="des">
+          <p>This is Netflix Series</p>
+          <button>Watch now</button>
+        </div>
+      </div>
+
+      <div class="cards">
+        <div class="image">
+          <img
+            src="https://occ-0-4995-2164.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABQ8UCIw9Po1eDYGwhQvkJkwpZqn2dApkBhXJnTKu-e-tF-AOkcfYzVpDBlJKGI2vNAuEeoAropV_M0j_FVHgRNXi6C9MdaMDqU1esuY-RcP7Zm0u560UV8bJJBkF72G7yC_K.jpg?r=088"
+            alt="placeholder"
+            height="200px"
+            width="100%"
+          />
+        </div>
+        <div class="title">
+          <p>Doli Saja Ke Rakhna</p>
+        </div>
+        <div class="des">
+          <p>This is Netflix Series</p>
+          <button>Watch now</button>
+        </div>
+      </div>
+
+      <div class="cards">
+        <div class="image">
+          <img
+            src="https://occ-0-4995-2164.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABSCzyyYPkXXkpm_UKXmLpZYa3DwgTcmfHfonGcPGZRgPKAlN6Ubr02eZMwhOxt_6n65r1Z1c3J2nK6LfL67P726ze_mvUvX7ujtFTOxWnmvbzBVMrNB61yoxMpXtBkPQKewHEeRELp9IiWFxU7P1bB74NO3ly3LCHUxksxO2-2lJKmurvLmMDhGKkrPRz0M.jpg?r=5da"
+            alt="placeholder"
+            height="200px"
+            width="100%"
+          />
+        </div>
+        <div class="title">
+          <p>Doli Saja Ke Rakhna</p>
+        </div>
+        <div class="des">
+          <p>This is Netflix Series</p>
+          <button>Watch now</button>
+        </div>
+      </div>
+
+      <div class="cards">
+        <div class="image">
+          <img
+            src="https://occ-0-4995-2164.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABZS3AP64J4immWPo4qapDB_xFrMvNdBEiLW-1fc99mdQ36iOq0t9uxCehFenm9nx7E6z2KWP8EU9cq0BHK_l_F_7YDP_3N_QnrG8EJR8uEo9ADSqC2qFLP1hov1ytT_b29Gw.jpg?r=7f6"
+            alt="placeholder"
+            height="200px"
+            width="100%"
+          />
+        </div>
+        <div class="title">
+          <p>Doli Saja Ke Rakhna</p>
+        </div>
+        <div class="des">
+          <p>This is Netflix Series</p>
+          <button>Watch now</button>
+        </div>
+      </div>
+
+      <div class="cards">
+        <div class="image">
+          <img
+            src="https://occ-0-4995-2164.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABc_QuOdHzJa9WwBiBlfEvpcFRKq9TAIgdJv0KoSilQZ8JMEsdCdQS4VGD6gwVg2CYCYNhTJIald5uVsXbMf-3QaF1k1fnFRbovQr6ddWUtkSOUzil_dtlD1wOIeD_pSI6pAx.jpg?r=c68"
+            alt="placeholder"
+            height="200px"
+            width="100%"
+          />
+        </div>
+        <div class="title">
+          <p>Doli Saja Ke Rakhna</p>
+        </div>
+        <div class="des">
+          <p>This is Netflix Series</p>
+          <button>Watch now</button>
+        </div>
+      </div>
+
+      <div class="cards">
+        <div class="image">
+          <img
+            src="https://occ-0-4995-2164.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABdTvju96AJ0AYn4ZZjHJcVfa5KmULVu6hDRmXWLL3wZF9XGwNGSjNsz1nx2M7mqJtNK0-I2Hpmy3V33pFL0Mk_cnMook0DoKsN0T7k8MMh0zYpTR7fAZan4lJd8abznMEsjpkV2viXHAsvsA522PiVE2SHXzGUsN-vlMjVWaGmHnlP4wIAsLKlVm_T1unr3-fmA8woQ0yTjWlD0KKi2CM5cm8MGmNQNr_cCnWl3i3J6YNNA4L4Xj1W_MUaIYAD44Pxi-gq0CaTcp11k6-ggFWHLNmhznqopXBWQOc1JKwm3SFrRVauZ_cfxJ_pU1VVM4DGAzCIMxoXA7vUTwWRQkPAjwqfVJqfc3qn3ABRRHyQYgJK-r4.jpg?r=ae6"
+            alt="placeholder"
+            height="200px"
+            width="100%"
+          />
+        </div>
+        <div class="title">
+          <p>Doli Saja Ke Rakhna</p>
+        </div>
+        <div class="des">
+          <p>This is Netflix Series</p>
+          <button>Watch now</button>
+        </div>
+      </div>
+
+      <div class="cards">
+        <div class="image">
+          <img
+            src="https://occ-0-4995-2164.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABcg9TuSaELytTMpfLNBbxPokSI47FYpoWXi2vbAvluwsIXZAQ_M2mBc6BoAfjahHiZGrJzGmCwBxfwNG_JYhQZ7Q5O_5UjdousyT7iLkqOFlzgNy1L2hZNDEeYKXfYw-GOw9.jpg?r=b69"
+            alt="placeholder"
+            height="200px"
+            width="100%"
+          />
+        </div>
+        <div class="title">
+          <p>Doli Saja Ke Rakhna</p>
+        </div>
+        <div class="des">
+          <p>This is Netflix Series</p>
+          <button>Watch now</button>
+        </div>
+      </div>
+
+      <div class="cards">
+        <div class="image">
+          <img
+            src="https://occ-0-4995-2164.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABaSul_dyl7Vp3dMc_7KoET72knNATy8bgAjoJj9KpYYeqK3RaxRd2-S1pIqDUVsb74gPp5fiAYNFV5iNq-LYmTLFuZBVn3cOmyRhtJL0bHAqejMOv3T0kslLRDKvH6WujDZd.jpg?r=4fc"
+            alt="placeholder"
+            height="200px"
+            width="100%"
+          />
+        </div>
+        <div class="title">
+          <p>Doli Saja Ke Rakhna</p>
+        </div>
+        <div class="des">
+          <p>This is Netflix Series</p>
+          <button>Watch now</button>
+        </div>
+      </div>
+
+      <div class="cards">
+        <div class="image">
+          <img
+            src="https://occ-0-4995-2164.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABRubkeyHfqSusedjdWFF8aG2lkTZPxr6srku_66EbEeFZL8gL8eNP3MixbNrRLqlKlr5aFhDeVsTo9wtLfz2MxTzGDGtzdVOcVJbM04zQkdSz6sBYGVbgZ6ZivD4O3AxfuUy.jpg?r=945"
+            alt="placeholder"
+            height="200px"
+            width="100%"
+          />
+        </div>
+        <div class="title">
+          <p>Doli Saja Ke Rakhna</p>
+        </div>
+        <div class="des">
+          <p>This is Netflix Series</p>
+          <button>Watch now</button>
+        </div>
+      </div>
+
+      <div class="cards">
+        <div class="image">
+          <img
+            src="https://occ-0-4995-2164.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABVuhqH5tvREn_CdDZ0Hd_h_2U1CNDHNnv_Y7deTApe81b1sei7C7MKvK4gWMpamlSXvo8AIIS5m94R6hwfQapa0x3fwvRPingY5u-pIbsGriC1xqthXsNZ0Wjaq-LdTJd6Bk.jpg?r=152"
+            alt="placeholder"
+            height="200px"
+            width="100%"
+          />
+        </div>
+        <div class="title">
+          <p>Doli Saja Ke Rakhna</p>
+        </div>
+        <div class="des">
+          <p>This is Netflix Series</p>
+          <button>Watch now</button>
+        </div>
+      </div>
+
+      <div class="cards">
+        <div class="image">
+          <img
+            src="https://occ-0-4995-2164.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABbDtAv6HGIaSQvwDsHe1I1D9DrXIUH21Lwq2QoRd1EH56yCvEoR3xfR-BFr6hrdRxIu3GSbdWS_ZQ83ns4_lETpTUXNKvNwQrTwlcrW2F9-sTmg782-IgQe6ZGSIeZVJ8HX9.jpg?r=5b9"
+            alt="placeholder"
+            height="200px"
+            width="100%"
+          />
+        </div>
+        <div class="title">
+          <p>Doli Saja Ke Rakhna</p>
+        </div>
+        <div class="des">
+          <p>This is Netflix Series</p>
+          <button>Watch now</button>
+        </div>
+      </div>
+
+      <div class="cards">
+        <div class="image">
+          <img
+            src="https://occ-0-4995-2164.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABZrMLnw8WE5hCJsepUt8asAgLAOczm8tGn3WGO3OvnZengYhUh7ZT3tncJ9z00fK6zlch9fBhVMKHQI77cCFiCHmxTQEDwpjuUBNiEzyxgCrRXNDQtECKDbrH5pR5s5oagYG.jpg?r=488"
+            alt="placeholder"
+            height="200px"
+            width="100%"
+          />
+        </div>
+        <div class="title">
+          <p>Doli Saja Ke Rakhna</p>
+        </div>
+        <div class="des">
+          <p>This is Netflix Series</p>
+          <button>Watch now</button>
+        </div>
+      </div>
+
+      <div class="cards">
+        <div class="image">
+          <img
+            src="https://occ-0-4995-2164.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABQUAB7P085DHqN30qQxWCFOEVcJoeWbIByNwvYFVTl14oBd-G4SwpQB1MQH5BG23x9LJaBUGv3IdIaJQb0QN-wkFQminLabsKe87C7ZF-ZjenrWSHD3saAqpk8PRaqDGAKiJ.jpg?r=4f8"
+            alt="placeholder"
+            height="200px"
+            width="100%"
+          />
+        </div>
+        <div class="title">
+          <p>Doli Saja Ke Rakhna</p>
+        </div>
+        <div class="des">
+          <p>This is Netflix Series</p>
+          <button>Watch now</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Netflixfile;
